6be735bcc5ceb94c535b573e35c4e4ec785a203d
Mohammed Jacksi <moh.jacksi@yahoo.com>
2020-09-01 18:26:49 +0300

    final+

--- app/JsonMind.php
+++ app/JsonMind.php
@@ -8,7 +8,8 @@ class JsonMind extends Model
 {
     //
 
-    static function getTheSolution($json){
+    static function getTheSolution($json)
+    {
         // convert json to array
         $array = json_decode($json, true);
 
@@ -18,24 +19,38 @@ class JsonMind extends Model
         // initialize an empty array [] inside to start empty collection
         $new_collection = collect([]);
 
+        // walk through each item in the collection
+        // and inverse the employee with file position
+        // to make the employee the key, and the file name as array value of it
         $collection->each(function ($item, $key) use ($new_collection) {
+            // file name
             $array_key = array_keys($item)[0];
+            // employee name
             $array_value = array_values($item)[0];
-            if (isset($new_collection[$array_value])) {
-                $temp_array = $new_collection[$array_value];
-                array_push($temp_array, $array_key);
-                $new_collection[$array_value] = $temp_array;
-            } else {
+
+            // if there is no array created for files inside an employee collection
+            // then just create it and put the first value of it!
+            // otherwise push the new file name to the array that already there!
+            if (!isset($new_collection[$array_value])) {
                 $new_collection[$array_value] = [$array_key];
+            } else {
+                // get the array from the previews iteration
+                $temp_array = $new_collection[$array_value];
+                // put the new value to the files array
+                array_push($temp_array, $array_key);
+                // set the new array to the current employee
+                $new_collection[$array_value] = $temp_array;
             }
         });
 
-        // sorted by the number of files (to get the same output order)
+        // sorted by the number of files (to result as given output order)
         $sorted = $new_collection->sortByDesc(function ($value, $key) {
             return count($value);
         });
 
+        // new array to manipulate the collection to be as the same as the given format
         $new_array = [];
+        // reformatting the sorted collection to be same as required
         foreach ($sorted as $key => $value) {
             $new_array[] = [
                 $key => $value,
